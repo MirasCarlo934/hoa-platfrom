c9b3e6d6bd5c5c9b6e33c546d513fc1e42ca7f34
Carlo Miras <mirascarlo.business@gmail.com>
2025-06-16 15:49:35 +0800

    Add EJS to render /visitor-info page

--- app.ts
+++ app.ts
@@ -9,6 +9,9 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- src/routes/visitor-info.ts
+++ src/routes/visitor-info.ts
@@ -1,6 +1,4 @@
 import { Request, Response } from 'express';
-import fs from 'fs';
-import path from 'path';
 import { decrypt } from '../utils/crypto';
 
 const visitorInfoHandler = (req: Request, res: Response) => {
@@ -23,36 +21,7 @@ const visitorInfoHandler = (req: Request, res: Response) => {
   } else {
     error = true;
   }
-  const htmlPath = path.join(__dirname, '../../public/visitor-info.html');
-  fs.readFile(htmlPath, 'utf8', (err, html) => {
-    if (err) {
-      res.status(500).send('Error loading visitor info page.');
-      return;
-    }
-    let renderedHtml = html;
-    if (!error && decoded) {
-      const visitorDetails = `
-        <dt>UUID</dt><dd>${decoded.uuid}</dd>
-        <dt>Name of Visitor</dt><dd>${decoded.visitorName}</dd>
-        <dt>Car Plate Number</dt><dd>${decoded.carPlate}</dd>
-        <dt>Person to Visit</dt><dd>${decoded.personToVisit}</dd>
-        <dt>Address to Visit</dt><dd>${decoded.addressToVisit}</dd>
-      `;
-      renderedHtml = html.replace(
-        /<dl id="visitor-details"><\/dl>/,
-        `<dl id="visitor-details">${visitorDetails}</dl>`
-      );
-      renderedHtml = renderedHtml.replace(/<script>[\s\S]*?<\/script>/, '');
-      res.send(renderedHtml);
-    } else {
-      renderedHtml = html.replace(
-        /<dl id="visitor-details"><\/dl>/,
-        '<dl id="visitor-details"><dt>Error</dt><dd>Invalid or missing data in QR code.</dd></dl>'
-      );
-      renderedHtml = renderedHtml.replace(/<script>[\s\S]*?<\/script>/, '');
-      res.status(400).send(renderedHtml);
-    }
-  });
+  res.render('visitor-info', { visitor: !error && decoded ? decoded : null });
 };
 
 export default visitorInfoHandler;
