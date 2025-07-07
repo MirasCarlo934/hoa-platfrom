0813003bc9555f5048d1f283810c8a2399b3be43
Carlo Miras <mirascarlo.business@gmail.com>
2025-07-07 12:57:55 +0800

    Add prisma setup

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -4,10 +4,6 @@
 import { Request } from 'express';
 export const JWT_SECRET = process.env.JWT_SECRET || 'default_jwt_secret';
 
-export function authenticate(username: string, password: string): boolean {
-  return password === 'password';
-}
-
 export function getToken(req: Request): string | null {
   return req.cookies && req.cookies['token'];
 }
