8637ff148a99d352aaf2918f5b60d233f313190b
Carlo Miras <mirascarlo.business@gmail.com>
2025-07-07 21:48:53 +0800

    Refactor authentication logic

--- src/handlers/authenticate.ts
+++ src/handlers/authenticate.ts
@@ -1,18 +1,36 @@
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../utils/cookies';
-import { authenticate } from '../services/auth';
+import prisma from '../utils/db';
+import User, { UserType } from '../types/user';
 
 export default async function authenticateHandler(req: Request, res: Response) {
   const { username, password } = req.body;
-  if (await authenticate(username, password)) {
+  try {
+    const userRaw: UserType | null = await prisma.user.findUnique({
+      where: { username: username }
+    });
+
+    if (!userRaw) {
+      throw new Error('User not found');
+    }
+
+    const user: User = new User(userRaw)
+    if (!user.validatePassword(password)) {
+      throw new Error('Invalid password');
+    }
+
     const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: '1h' });
     res.cookie('token', token, { httpOnly: true });
-    res.cookie('user', {
-      username: username,
-    });
+    res.cookie('user', user.toJSON());
     res.redirect('/');
-  } else {
-    res.status(401).render('login', { error: 'Invalid username or password' });
+    
+  } catch (error) {
+    let errorMsg = error;
+    if (error instanceof Error) {
+      errorMsg = error.message;
+    }
+    console.error('Authentication failed:', errorMsg);
+    res.status(401).render('login', { error: errorMsg });
   }
 }
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,11 +1,55 @@
-export default class User {
-  private username: string;
+export type UserType = {
+  username: string;
+  password: string;
+  role: string;
+  firstname: string;
+  lastname: string;
+};
 
-  constructor(username: string) {
-    this.username = username;
+export type UserWithoutPasswordType = {
+  username: string;
+  role: string;
+  firstname: string;
+  lastname: string;
+};
+
+export default class User implements UserType {
+  username: string;
+  password: string;
+  role: string;
+  firstname: string;
+  lastname: string;
+
+  constructor(obj: UserType) {
+    this.username = obj.username;
+    this.password = obj.password;
+    this.role = obj.role;
+    this.firstname = obj.firstname;
+    this.lastname = obj.lastname;
   }
 
   getUsername(): string {
     return this.username;
+  }
+
+  getRole(): string {
+    return this.role;
+  }
+
+  getFullName(): string {
+    return `${this.firstname} ${this.lastname}`;
+  }
+
+  validatePassword(password: string): boolean {
+    return this.password === password;
+  }
+
+  toJSON(): UserWithoutPasswordType {
+    return {
+      username: this.username,
+      role: this.role,
+      firstname: this.firstname,
+      lastname: this.lastname
+    };
   }
 }
