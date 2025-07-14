f697f66569f3690b1b6653ea8f03408b7f57179c
Carlo Miras <mirascarlo.business@gmail.com>
2025-07-14 21:50:21 +0800

    Expose POST /generate-qr as public route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import homeHandler from "../handlers/home";
-import generateQrHandler from "../handlers/generate-qr";
 
 const indexRouter = Router();
 
 indexRouter.get('/', homeHandler);
-indexRouter.post('/generate-qr', generateQrHandler);
 
 export default indexRouter;
--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import visitorInfoHandler from "../handlers/visitor-info";
+import generateQrHandler from "../handlers/generate-qr";
 
 export const visitorInfoRoute = '/visitor-info';
 
 const publicRouter = Router();
 
+publicRouter.post('/generate-qr', generateQrHandler);
 publicRouter.get(visitorInfoRoute, visitorInfoHandler);
 
 export default publicRouter;
