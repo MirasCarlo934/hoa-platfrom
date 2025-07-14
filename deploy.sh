b03dbaa944ebb5155c04f170aef6501e8d1a4cf3
Carlo Miras <mirascarlo.business@gmail.com>
2025-07-14 21:48:30 +0800

    Update deploy.sh and docker-compose.yml

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
-docker build -t cogaenic/c8yhub:latest .
+#!/bin/bash
+
+docker build --platform=linux/amd64 -t cpmiras/c8yhub:latest .
+docker push cpmiras/c8yhub:latest
+
+# # SFTP upload docker-compose.yml to remote server
+# sftp user@your.remote.host <<EOF
+# put docker-compose.yml /remote/path/to/docker-compose.yml
+# bye
+# EOF
